b90dafd07357f205010ffd3af543bf49dd50ff34
Laia <laia.alonso.romero@gmail.com>
2025-05-23 16:54:20 +0200

    header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ function Header() {
     <>
         <div id="header_espacio_nav">
         <ul class="maintabs">
+            {/* <img src="public/cnnLogoRed.png" alt="Logo CNN"> */}
             <li>World</li>
             <li>Politics</li>
             <li>Business</li>
