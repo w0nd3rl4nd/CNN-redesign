0d7652087a51bf6295cd0e46ff085362b6a62cd7
Laia <laia.alonso.romero@gmail.com>
2025-05-23 15:45:37 +0200

    prueba

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './NewsCard.css'
 
 const styles = {
   breaking: {
@@ -10,7 +9,7 @@ const styles = {
     button: { padding: "10px 20px", fontSize: 16, backgroundColor: "red", color: "white", border: "none", cursor: "pointer" },
   },
   currently: {
-    container: { border: "1.5px solid blue", padding: 15, maxWidth: 400, margin: "auto", textAlign: "center" },
+    container: { border: "1.5px solid black", padding: 15, maxWidth: 400, margin: "auto", textAlign: "center" },
     image: { width: "100%", height: 180, objectFit: "cover" },
     title: { fontSize: 20, fontWeight: "600", marginTop: 10 },
   },
@@ -30,19 +29,13 @@ export function NewsCard({ type, image, title, description, link }) {
   switch (type) {
     case "breaking":
       return (
-      <div className="breaking-container">
         <div style={styles.breaking.container}>
-          <div className="img-breaking">
-            <img src={image} alt={title} style={styles.breaking.image} />
-          </div>
-          <div className="text-breaking">
-            <h2 style={styles.breaking.title}>{title}</h2>
-            <p style={styles.breaking.description}>{description}</p>
-            <a href={link} target="_blank" rel="noopener noreferrer">
-              <button style={styles.breaking.button}>Read More</button>
-            </a>
-          </div>
-        </div>
+          <img src={image} alt={title} style={styles.breaking.image} />
+          <h2 style={styles.breaking.title}>{title}</h2>
+          <p style={styles.breaking.description}>{description}</p>
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <button style={styles.breaking.button}>Read More</button>
+          </a>
         </div>
       );
 
