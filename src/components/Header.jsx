167e7f1d0d4f4178dfc33ee025ce929b5d26a378
Laia <laia.alonso.romero@gmail.com>
2025-05-23 16:08:29 +0200

    Create Header jsx & css

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+        <div id="header_espacio_nav">
+            <li>World</li>
+            <li>Politics</li>
+            <li>Business</li>
+            <li>Health</li>
+            <li>Entertainment</li>
+        </div>
+    </>
+  );
+}
